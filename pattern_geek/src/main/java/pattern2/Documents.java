97459ea09e2ffaf41ed23adf5daac8fc98616593
Koluskin Alexander <kolushkin@gmail.com>
2018-10-16 14:58:52 +0300

    add singltone

--- pattern_geek/src/main/java/pattern2/Documents.java
+++ pattern_geek/src/main/java/pattern2/Documents.java
@@ -5,14 +5,25 @@ import java.util.List;
 
 public class Documents {
 
-    private static List<Document> list;
-    private static Documents ourInstance = new Documents();
+    private List<Document> list;
+    private static Documents ourInstance = null;
 
     public static Documents getInstance() {
+        if(ourInstance == null) ourInstance = new Documents();
         return ourInstance;
     }
 
     private Documents() {
-       if(list == null) list = new ArrayList<Document>();
+       list = getDocuments();
+    }
+
+    private List<Document> getDocuments(){
+        //FIXME
+        //get a list of documents from DB
+         return new ArrayList<Document>();
+    }
+
+    public List<Document> getList() {
+        return list;
     }
 }
